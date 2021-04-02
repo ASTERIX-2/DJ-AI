3983eebbac47525bc5394b00599a90a95f24fbf4
ASTERIX-2 <78957064+ASTERIX-2@users.noreply.github.com>
2021-04-02 15:37:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-var sound="";
-function preload(){
-sound =loadSound("music.mp3");
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+var sound = "";
+function preload() {
+    sound = loadSound("music.mp3");
 }
-function setup(){
-canvas=createCanvas(600,500);
-canvas.center();
-video=createCapture(VIDEO);
-video.hide()
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide()
+    posenet =ml5.poseNet(video,modelLoaded);
+    posenet.on("pose",gotPoses)
 }
-function draw(){
-image(video,0,0,600,500);
+function draw() {
+    image(video, 0, 0, 600, 500);
 }
-function play(){
+function play() {
     sound.play();
+    sound.setVolume(1);
+    sound.rate(1);
+}
+function modelLoaded(){
+    console.log("posenet is initialized");
+}
+function gotPoses(results){
+    if (results.length>0) {
+        console.log(results);
+        leftWristX= results[0].pose.leftWrist.x;
+        leftWristY= results[0].pose.leftWrist.y;
+        console.log("leftWristX="+leftWristX+" leftWristY="+leftWristY);
+        rightWristX= results[0].pose.rightWrist.x;
+        rightWristY= results[0].pose.rightWrist.y;
+        console.log("rightWristX="+rightWristX+" rightWristY="+rightWristY);
+    }
 }
